e1736af3c0f10a3a062752595decc536b9cf6e5d
Patryk Gamrat <Patryk.gam@gmail.com>
2023-01-06 02:50:55 +0100

    Difficulty adjustments.

--- GalaxyInvaders.h
+++ GalaxyInvaders.h
@@ -17,8 +17,8 @@ const int FRAME_RATE = 60;
 
 //Player
 const float PLAYER_SPEED = 5.f;
-const float BULLET_SPEED = 5.f;
-const float BULLET_DELAY = 0.2f;
+const float BULLET_SPEED = 3.f;
+const float BULLET_DELAY = 0.3f;
 
 //Aliens
 const float ALIEN_SPEED = 1.f;
@@ -26,5 +26,5 @@ const int ALIEN_COL = 10;
 const int ALIEN_ROW = 6;
 const float ALIEN_WIDTH = 44.f;
 const float ALIEN_HEIGHT = 32.f;
-const float ALIEN_BULLET_SPEED = 5.f;
+const float ALIEN_BULLET_SPEED = 3.f;
 const float ALIEN_BULLET_DELAY = 1.f;
